f7b7cedbd2231de3b0a733f93113214d68b8bfb7
mayor167 <adenijimayokun@gmail.com>
2025-06-14 05:56:53 +0100

    virtual card design completed

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -1,12 +1,41 @@
+import 'package:dashboard_project_new/utilis/app_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class NotificationScreen extends StatelessWidget {
   const NotificationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      backgroundColor: Colors.black87,
+          body:ListView(
+                    children: [
+                          Container(
+                            child: Column(
+                              children: [
+                                CircleAvatar(
+                                  maxRadius: 30,
+                                  backgroundImage: AssetImage("assets/images/Mayor1.png"),
+                                ),
+                                Text("Welcome Back", style: AppStyles.headLineStyle1,), 
+                                SizedBox(height:10 ,),
+                                Text("Oyeyemi Mayokun", style: AppStyles.headLineStyle3,), 
+                                SizedBox(height:15 ,),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(Icons.key, color: Colors.white,),
+                                    SizedBox(width:5 ,),
+                                    Text("Passcode", style: TextStyle(color: Colors.grey),)
+                                  ],
+                                )
+
+                              ],
+                            ),
+                          ),
+                    ],
+          )
     );
   }
 }
